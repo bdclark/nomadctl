d711b54d12f47ca4614a757b79e41112f665edee
Brian Clark <brian@clark.zone>
2018-01-25 18:58:49 -0500

    set next dev version

--- version/version.go
+++ version/version.go
@@ -10,12 +10,12 @@ var (
 	GitCommit string
 
 	// Version is the main version number that is being run at the moment.
-	Version = "0.0.1"
+	Version = "0.0.2"
 
 	// VersionPrerelease is a pre-release marker for the version. If this is "" (empty string)
 	// then it means that it is a final release. Otherwise, this is a pre-release
 	// such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = ""
+	VersionPrerelease = "dev"
 )
 
 // Get returns the version number
